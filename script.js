a7d5c54f87feceb923b05603cb671992c5151257
KidNit <tinashegumbo@gmail.com>
2026-05-16 23:42:06 +0530

    At this point I dont even understand what I am doing

--- script.js
+++ script.js
@@ -26,8 +26,8 @@ function getComputerChoice(max){
 
     return Math.floor(Math.random() * max);
 }
-getComputerChoice(3);
-
+let debugging =getComputerChoice(3);
+console.log(debugging);
 function getHumanChoice(){
     let Choice = prompt("Please enter your choice: ").toLowerCase();
     if (Choice !== rock && Choice !== paper && Choice !== scissors){
@@ -39,21 +39,26 @@ getHumanChoice();
 
 function gameLogic(humanScore, computerScore){
     if(getComputerChoice=0){
-        // console.log("Rock");
         getComputerChoice = rock;
     }else if(getComputerChoice=1){
-        // console.log("Paper");
         getComputerChoice = paper;
     }else if(getComputerChoice=2){
-        // console.log("Scissors");
         getComputerChoice = scissors;
     }
     if(humanScore=computerScore){
         prompt("This Is A Draw");
-    }else if(humanScore>computerScore){
-        console.log(computerScore);
+    }else if(humanScore == rock && computerScore==scissors || humanScore==scissors && computerScore==paper ){
+        console.log("playerScore++ ");
+        let p = 1;
+    }else if(computerScore == rock && humanScore==scissors || computerScore==scissors && humanScore==paper){
+        console.log("computerScore++ ");
+        let c =1;
     }
 }
 gameLogic(getComputerChoice, getHumanChoice);
 
+function scoreline(){
+    gameLogic()
+    while()
+};
 
